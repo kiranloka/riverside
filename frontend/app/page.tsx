868ab6ee2ca63454350ccf80db39cdd5087e6a70
kiranLoka <kirankumarloka@gmail.com>
2025-05-05 16:24:17 +0530

    wrote schema

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="text-xl flex justify-center text-bold text-white">
+      <div
+        className="text-white border-r-2 border-white
+       border-l-2"
+      >
+        text fr0om iiran how much ever typr thea much ever yo see
+      </div>
+    </div>
+  );
+}
